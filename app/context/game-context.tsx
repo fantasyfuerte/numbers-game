de755a688167bf53c28a82dbd36164b85f6d0a9b
Fuerte <leandrofuerte75@gmail.com>
2025-04-10 22:48:52 -0400

    Refactor GameProvider to set initial onlinePeople state and comment out socket connection logic

--- app/context/game-context.tsx
+++ app/context/game-context.tsx
@@ -8,21 +8,22 @@ export const GameContext = createContext({
 });
 
 export function GameProvider({ children }: { children: React.ReactNode }) {
+  const [onlinePeople, setOnlinePeople] = useState(0);
 
-    const [onlinePeople, setOnlinePeople] = useState(0);
+  setOnlinePeople(1);
 
-    useEffect(() => {
-        const socket = new Socket("https://numbers-game-fppg.onrender.com");
-        socket.on("connect", () => {
-            console.log("connected");
-        });
-        socket.on("disconnect", () => {
-            console.log("disconnected");
-        });
-        socket.on("onlinePeople", (data) => {
-            setOnlinePeople(data);
-        });
-    }, []);
+  // useEffect(() => {
+  //     const socket = new Socket("https://numbers-game-fppg.onrender.com");
+  //     socket.on("connect", () => {
+  //         console.log("connected");
+  //     });
+  //     socket.on("disconnect", () => {
+  //         console.log("disconnected");
+  //     });
+  //     socket.on("onlinePeople", (data) => {
+  //         setOnlinePeople(data);
+  //     });
+  // }, []);
 
   return (
     <GameContext.Provider value={{ onlinePeople: onlinePeople }}>
