19099238b20bc9cffe25776a43e2984acb74fa78
Fuerte <leandrofuerte75@gmail.com>
2025-04-10 20:50:32 -0400

    Home Page designed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-import Image from "next/image";
+import { Saira_Stencil_One } from "next/font/google";
+
+const sairaStencilOne = Saira_Stencil_One({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center">
+        <h4 className="text-xl text-primary font-bold">Welcome to</h4>
+        <h1
+          className={`text-6xl font-bold gradient-text ${sairaStencilOne.className}`}
+        >
+          Numbers Game
+        </h1>
+      </div>
+    </main>
   );
 }
