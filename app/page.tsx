fb128a3422f5fda1c80cabb0a5e565ae5dbf838f
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 18:35:56 -0400

    Add Waiting component to handle game waiting state and manage match cancellation

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useGame } from "./context/game-context";
 import Initial from "./stages/1-initial";
+import Waiting from "./stages/2-waiting-rival";
 
 enum Stages {
   INITIAL = "initial",
@@ -12,18 +13,25 @@ enum Stages {
 
 export default function Home() {
   const [appStage, setAppStage] = useState<Stages>(Stages.INITIAL);
+  const [code, setCode] = useState<string>("");
 
   const { createGame } = useGame();
 
   function createMatch() {
     setAppStage(Stages.WAITING);
     const code = crypto.randomUUID().slice(0, 5);
+    setCode(code);
     createGame(code);
+  }
+
+  function cancelMatch() {
+    setAppStage(Stages.INITIAL);
   }
 
   return (
     <main className="flex flex-col items-center gap-30 md:p-20 pt-24 md:pt-20">
       {appStage === Stages.INITIAL && <Initial createGame={createMatch} />}
+      {appStage === Stages.WAITING && <Waiting code={code} cancelMatch={cancelMatch} />}
     </main>
   );
 }
