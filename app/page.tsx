ea212e9e9e80b71bd81d95a9aa154ef6836dc09d
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 17:21:46 -0400

    Refactor Home component to restore game cards and welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Saira_Stencil_One } from "next/font/google";
-import GameCards from "./components/game-cards";
 
 const sairaStencilOne = Saira_Stencil_One({
   weight: "400",
@@ -9,28 +8,7 @@ const sairaStencilOne = Saira_Stencil_One({
 export default function Home() {
   return (
     <main className="flex flex-col items-center gap-30 md:p-20 pt-24 md:pt-20">
-      <div className="flex flex-col items-center -mt-10">
-        <h4 className="text-lg md:text-xl text-primary font-bold">
-          Welcome to
-        </h4>
-        <h1
-          className={`text-5xl md:text-6xl font-bold gradient-text ${sairaStencilOne.className}`}
-        >
-          Numbers Game
-        </h1>
-      </div>
-      <section className="flex gap-4 justify-around flex-wrap">
-        <GameCards
-          title="Create New Game"
-          description="Invite a friend to guess your number. The first one to guess wins"
-          cta="Create"
-        />
-        <GameCards
-          title="Join Game"
-          description="Join a game created by a friend"
-          cta="Join"
-        />
-      </section>
+      
     </main>
   );
 }
---
+++ app/stages/Home.tsx
@@ -0,0 +1,33 @@
+import { Saira_Stencil_One } from "next/font/google";
+import GameCards from "@/app/components/game-cards";
+
+function Home() {
+  return (
+    <>
+      <div className="flex flex-col items-center -mt-10">
+        <h4 className="text-lg md:text-xl text-primary font-bold">
+          Welcome to
+        </h4>
+        <h1
+          className={`text-5xl md:text-6xl font-bold gradient-text ${sairaStencilOne.className}`}
+        >
+          Numbers Game
+        </h1>
+      </div>
+      <section className="flex gap-4 justify-around flex-wrap">
+        <GameCards
+          title="Create New Game"
+          description="Invite a friend to guess your number. The first one to guess wins"
+          cta="Create"
+        />
+        <GameCards
+          title="Join Game"
+          description="Join a game created by a friend"
+          cta="Join"
+        />
+      </section>
+    </>
+  );
+}
+
+export default Home;
