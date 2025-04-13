2ee832d897869b2966dd6417200270a376451ae7
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 18:36:04 -0400

    Add Waiting component to display waiting state and allow match cancellation

---
+++ app/stages/2-waiting-rival.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface Props {
+  code: string;
+  cancelMatch: () => void;
+}
+
+function Waiting({ code, cancelMatch }: Props) {
+  return (
+    <article className="rounded-xl p-4 flex flex-col items-center gap-8">
+      <h3 className="text-primary font-bold text-center">Waiting for rival</h3>
+      <h3 className="text-primary font-bold text-center">{code}</h3>
+      <button className="bg-primary hover:bg-primary/80 text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit">
+        Cancel
+      </button>
+    </article>
+  );
+}
+
+export default Waiting;
