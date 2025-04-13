e6fb3bf5a5b771207ce48ccde01779f5f3ae5141
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 17:01:46 -0400

    Refactor GameContext to enhance type safety and improve game state management

--- app/context/game-context.tsx
+++ app/context/game-context.tsx
@@ -2,14 +2,19 @@
 
 import { createContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import Image from "next/image";
 
 const socket = io("https://numbers-game-fppg.onrender.com/");
 
-export const GameContext = createContext({
+export const GameContext = createContext<ReturnType<typeof getGame>>({
   onlinePeople: 0,
+  createGame: () => {},
+  joinGame: () => {},
+  setSecretNumber: () => {},
+  endGame: () => {},
 });
 
-export function GameProvider({ children }: { children: React.ReactNode }) {
+const getGame = () => {
   const [onlinePeople, setOnlinePeople] = useState(0);
 
   useEffect(() => {
@@ -17,10 +22,23 @@ export function GameProvider({ children }: { children: React.ReactNode }) {
       setOnlinePeople(socket);
     });
   }, []);
+  const createGame = (code: string) => {
+    socket.emit("create-game", code);
+  };
 
+  const joinGame = (code: string) => {
+    socket.emit("join-game", code);
+  };
+
+  const setSecretNumber = (number: number, gameCode: string) => {
+    socket.emit("set-secret-number", number, gameCode);
+  };
+
+  return { onlinePeople, createGame };
+};
+
+export function GameProvider({ children }: { children: React.ReactNode }) {
   return (
-    <GameContext.Provider value={{ onlinePeople: onlinePeople }}>
-      {children}
-    </GameContext.Provider>
+    <GameContext.Provider value={getGame()}>{children}</GameContext.Provider>
   );
 }
