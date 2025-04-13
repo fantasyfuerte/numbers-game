350820b337fa722102453fc4fe4af609bc3d665b
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 17:34:53 -0400

    Fix background color in GameCards component for improved visual consistency

--- app/components/game-cards.tsx
+++ app/components/game-cards.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function GameCards({ title, description, cta }: Props) {
   return (
-    <article className="border-primary/50 border-2 rounded-lg p-4 flex flex-col gap-16 w-[300px] justify-between bg-gradient-to-b odd:from-backgroundSecondary/70 even:from-backgroundPrimary/70 to-white/15 shadow-primary/20 shadow-2xl hover:scale-110 hover:z-20 transition duration-700">
+    <article className="border-primary/50 border-2 rounded-lg p-4 flex flex-col gap-16 w-[300px] justify-between bg-gradient-to-b odd:from-backgroundSecondary/70 even:from-backgroundPrimary/70 to-primary/20 shadow-primary/20 shadow-2xl hover:scale-110 hover:z-20 transition duration-700">
       <div>
         <h4 className="text-primary font-bold">{title}</h4>
         <p className="text-primary/70 font-semibold">{description}</p>
