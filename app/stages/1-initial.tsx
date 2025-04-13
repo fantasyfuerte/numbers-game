b9e91bcea8a1ef945c718abc7a62aa3fe822ebf7
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 19:10:09 -0400

    Refactor HomePage component structure for improved readability

--- app/stages/1-initial.tsx
+++ app/stages/1-initial.tsx
@@ -15,30 +15,38 @@ function HomePage({ createGame }: Props) {
   const [modal, setModal] = useState<boolean>(false);
   return (
     <>
-      <div className="flex flex-col items-center -mt-10">
-        <h4 className="text-lg md:text-xl text-primary font-bold">
-          Welcome to
-        </h4>
-        <h1
-          className={`text-5xl md:text-6xl font-bold gradient-text ${sairaStencilOne.className}`}
-        >
-          Numbers Game
-        </h1>
-      </div>
-      <section className="flex gap-8 justify-around flex-wrap">
-        <GameCards
-          title="Create New Game"
-          description="Invite a friend to guess your number. The first one to guess wins"
-          cta="Create"
-          action={createGame}
-        />
-        <GameCards
-          title="Join Game"
-          description="Join a game created by a friend"
-          cta="Join"
-          action={() => setModal(true)}
-        />
-      </section>
+      {modal ? (
+        <div className="">
+          <h4 className="text-primary text-2xl font-bold">Enter a game code</h4>
+        </div>
+      ) : (
+        <>
+          <div className="flex flex-col items-center -mt-10">
+            <h4 className="text-lg md:text-xl text-primary font-bold">
+              Welcome to
+            </h4>
+            <h1
+              className={`text-5xl md:text-6xl font-bold gradient-text ${sairaStencilOne.className}`}
+            >
+              Numbers Game
+            </h1>
+          </div>
+          <section className="flex gap-8 justify-around flex-wrap">
+            <GameCards
+              title="Create New Game"
+              description="Invite a friend to guess your number. The first one to guess wins"
+              cta="Create"
+              action={createGame}
+            />
+            <GameCards
+              title="Join Game"
+              description="Join a game created by a friend"
+              cta="Join"
+              action={() => setModal(true)}
+            />
+          </section>
+        </>
+      )}
     </>
   );
 }
