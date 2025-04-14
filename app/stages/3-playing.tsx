9020b7dbc66ec8ee27204e58fc88a30877f3682a
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 23:27:59 -0400

    Update Playing component to prompt user for secret number selection

--- app/stages/3-playing.tsx
+++ app/stages/3-playing.tsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
+import { Numbers } from "../components/number-setter.tsx";
 
 function Playing() {
   const [secretNumber, setSecretNumber] = useState<null | number>(null);
 
-  return <>
-    {!secretNumber && (
-      <div className="flex flex-col items-center">
-        <h4 className="text-3xl md:text-4xl font-bold text-primary">
-          Waiting for the rival to choose a secret number
-        </h4>
-      </div>
-    )}
-  </>;
+  return (
+    <>
+      {!secretNumber && (
+        <div className="flex flex-col items-center">
+          <h4 className="text-2xl md:text-3xl font-bold text-primary mb-10">
+            Choose your secret number
+          </h4>
+          <Numbers />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Playing;
