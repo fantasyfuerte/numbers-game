458e7e60bad8e41560d6475e39c2d6b4f64e2391
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 23:33:58 -0400

    Update Playing component to manage secret number state and ready status

--- app/stages/3-playing.tsx
+++ app/stages/3-playing.tsx
@@ -2,16 +2,17 @@ import { useState } from "react";
 import { Numbers } from "../components/number-setter.tsx";
 
 function Playing() {
-  const [secretNumber, setSecretNumber] = useState<null | number>(null);
+  const [secretNumber, setSecretNumber] = useState<number>();
+  const [readyToPlay, setReadyToPlay] = useState(false);
 
   return (
     <>
-      {!secretNumber && (
+      {!readyToPlay && (
         <div className="flex flex-col items-center">
           <h4 className="text-2xl md:text-3xl font-bold text-primary mb-10">
             Choose your secret number
           </h4>
-          <Numbers />
+          <Numbers setSecretCode={setSecretNumber} />
         </div>
       )}
     </>
