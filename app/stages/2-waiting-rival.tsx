0cf707f41f81ce672a071721f5bfeeb4d2ddefbc
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 23:15:12 -0400

    Refactor Waiting component to use h4 tags for consistency; implement secret number state in Playing component for game logic.

--- app/stages/2-waiting-rival.tsx
+++ app/stages/2-waiting-rival.tsx
@@ -8,10 +8,10 @@ interface Props {
 function Waiting({ code, cancelMatch }: Props) {
   return (
     <article className="rounded-xl p-4 flex flex-col items-center gap-8">
-      <h3 className="text-primary font-bold text-center">Waiting for rival</h3>
-      <h3 className="text-primary font-bold text-center text-3xl gradient-text">
+      <h4 className="text-primary font-bold text-center">Waiting for rival</h4>
+      <h4 className="text-primary font-bold text-center text-3xl gradient-text">
         {code}
-      </h3>
+      </h4>
       <button
         className="bg-primary hover:bg-primary/80 text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit"
         onClick={cancelMatch}
--- app/stages/3-playing.tsx
+++ app/stages/3-playing.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { useState } from "react";
 
 function Playing() {
-  return <div>Playing</div>;
+  const [secretNumber, setSecretNumber] = useState<null | number>(null);
+
+  return <>
+    {!secretNumber && (
+      <div className="flex flex-col items-center">
+        <h4 className="text-3xl md:text-4xl font-bold text-primary">
+          Waiting for the rival to choose a secret number
+        </h4>
+      </div>
+    )}
+  </>;
 }
 
 export default Playing;
