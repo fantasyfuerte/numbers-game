b286f2bd9a2cb415e8d615c33044db5d3643a0b0
Fuerte <leandrofuerte75@gmail.com>
2025-04-14 00:25:27 -0400

    Refactor Playing component to accept code as a prop and update secret number handling

--- app/stages/3-playing.tsx
+++ app/stages/3-playing.tsx
@@ -1,12 +1,22 @@
 import { useEffect, useState } from "react";
 import { Numbers } from "../components/number-setter.tsx";
+import { useGame } from "../context/game-context.jsx";
 
-function Playing() {
-  const [secretNumber, setSecretNumber] = useState<number>();
+interface Props {
+  code: string;
+}
+
+function Playing({ code }: Props) {
+  const [choosenNumber, setChosenNumber] = useState<number>();
   const [readyToPlay, setReadyToPlay] = useState(false);
 
-  useEffect(() => {
-  }, [secretNumber]);
+  const { setSecretNumber } = useGame();
+
+  function Submit() {
+    if (choosenNumber) {
+      setSecretNumber(choosenNumber, code);
+    }
+  }
 
   return (
     <>
@@ -15,7 +25,13 @@ function Playing() {
           <h4 className="text-2xl md:text-3xl font-bold text-primary mb-10">
             Choose your secret number
           </h4>
-          <Numbers setSecretCode={setSecretNumber} />
+          <Numbers setSecretCode={setChosenNumber} />
+          <button
+            className="bg-primary hover:bg-primary/80 text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit mx-auto mt-4 block"
+            onClick={Submit}
+          >
+            Submit
+          </button>
         </div>
       )}
     </>
