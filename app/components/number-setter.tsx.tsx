5ffa9256a431c95106e9381e68dabddb31143ada
Fuerte <leandrofuerte75@gmail.com>
2025-04-13 23:28:12 -0400

    Add Numbers component for user input validation and display

---
+++ app/components/number-setter.tsx.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+export function Numbers(props: NumbersProp) {
+  const [value, setValue] = useState("");
+  const [focused, setFocused] = useState(false);
+  const validate = (value: string) => {
+    if (value.length > 4) return false;
+    if (value.startsWith("0")) return false;
+    if (new Set(value).size != value.length) return false;
+    return /^[0-9]*$/.test(value);
+  };
+  const values = value.split("");
+  const length = values.length;
+  const onFocus = () => setFocused(true);
+  const onBlur = () => setFocused(false);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const validationResult = validate(e.target.value);
+    if (!validationResult) {
+      e.preventDefault();
+      return;
+    }
+    setValue(e.target.value);
+    props.onChange?.(e.target.value);
+  };
+  return (
+    <label className="relative grid grid-cols-4 gap-2 max-w-full w-xs text-xl font-bold text-primary">
+      <input
+        type="text"
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onChange={onChange}
+        value={value}
+        className="opacity-0 absolute"
+        minLength={4}
+        maxLength={4}
+        required
+        disabled={props.disabled}
+      />
+      <div className="flex items-center justify-center aspect-square border-2 border-primary/70 rounded-md">
+        {values[0] && values[0]}
+        {length == 0 && focused && <Caret />}
+      </div>
+      <div className="flex items-center justify-center aspect-square border-2 border-primary/70 rounded-md">
+        {values[1] && values[1]}
+        {length == 1 && focused && <Caret />}
+      </div>
+      <div className="flex items-center justify-center aspect-square border-2 border-primary/70 rounded-md">
+        {values[2] && values[2]}
+        {length == 2 && focused && <Caret />}
+      </div>
+      <div className="flex items-center justify-center aspect-square border-2 border-primary/70 rounded-md">
+        {values[3] && values[3]}
+        {length >= 3 && focused && <Caret />}
+      </div>
+    </label>
+  );
+}
+
+function Caret() {
+  return <div className="h-[70%] w-[2px] bg-base-content animate-pulse"></div>;
+}
+
+interface NumbersProp {
+  onChange?: (number: string) => void;
+  disabled?: boolean;
+}
