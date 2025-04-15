c49fe4dd687ed8173d98f8ee0b4aa215a1448cdd
Fuerte <leandrofuerte75@gmail.com>
2025-04-15 16:10:07 -0400

    Fix style

--- app/components/join-game-modal.tsx
+++ app/components/join-game-modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 interface Props {
   code: string;
@@ -24,20 +24,22 @@ function JoinGameModal({ code, setCode, cancel, joinGame }: Props) {
       <h4 className="text-primary text-2xl font-bold text-center">
         Match Code
       </h4>
-      <input
-        type="text"
-        placeholder="Type code here..."
-        className="border-2 border-primary rounded-lg px-4 py-2 mt-4 outline-none font-bold gradient-text text-center placeholder:opacity-45"
-        onChange={(e) => setCode(e.target.value)}
-        value={code}
-        autoFocus
-      />
-      <button
-        className="bg-primary hover:bg-primary/80 text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit mx-auto mt-4 block"
-        onClick={joinMatch}
-      >
-        Join
-      </button>
+      <article>
+        <input
+          type="text"
+          placeholder="Type code here..."
+          className="border-2 border-primary rounded-lg px-4 py-2 mt-4 outline-none font-bold gradient-text text-center placeholder:opacity-45"
+          onChange={(e) => setCode(e.target.value)}
+          value={code}
+          autoFocus
+        />
+        <button
+          className="bg-primary hover:bg-primary/80 text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit mx-auto mt-4 block"
+          onClick={joinMatch}
+        >
+          Join
+        </button>
+      </article>
       <button
         className="bg-primary hover:bg-primary/80 text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit mx-auto mt-4 block"
         onClick={() => {
