1cae8ee065729527f9f7cfbc42126951848d175a
Fuerte <leandrofuerte75@gmail.com>
2025-04-15 16:12:10 -0400

    Fix style

--- app/components/join-game-modal.tsx
+++ app/components/join-game-modal.tsx
@@ -24,17 +24,17 @@ function JoinGameModal({ code, setCode, cancel, joinGame }: Props) {
       <h4 className="text-primary text-2xl font-bold text-center">
         Match Code
       </h4>
-      <article>
+      <article className="flex gap-2 mt-4">
         <input
           type="text"
           placeholder="Type code here..."
-          className="border-2 border-primary rounded-lg px-4 py-2 mt-4 outline-none font-bold gradient-text text-center placeholder:opacity-45"
+          className="border-2 border-primary rounded-lg px-4 py-2 outline-none font-bold gradient-text text-center placeholder:opacity-45"
           onChange={(e) => setCode(e.target.value)}
           value={code}
           autoFocus
         />
         <button
-          className="bg-primary hover:bg-primary/80 text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit mx-auto mt-4 block"
+          className="bg-primary hover:bg-primary/80 text-backgroundSecondary p-4 rounded-lg font-bold w-fit mx-auto"
           onClick={joinMatch}
         >
           Join
