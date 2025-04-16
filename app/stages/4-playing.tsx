6e8e8f2c4ead9e9dccde9360f732996529664b57
Fuerte <leandrofuerte75@gmail.com>
2025-04-16 11:51:52 -0400

    Add submit button to Playing component for number submission

--- app/stages/4-playing.tsx
+++ app/stages/4-playing.tsx
@@ -34,6 +34,16 @@ function Playing({ code }: Props) {
           submitted={submitted}
           disabled={submitted}
         />
+        <button
+          type="submit"
+          className={`text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit mx-auto mt-4 block ${
+            !number || number?.length !== 4
+              ? "opacity-40 bg-none"
+              : "bg-primary hover:bg-primary/80"
+          }`}
+        >
+          Submit
+        </button>
       </form>
       <article className="grid grid-cols-2 gap-4">
         <div>
