c24304caaf253eef9cbefb5ad9c239fece1fb694
Fuerte <leandrofuerte75@gmail.com>
2025-04-16 12:35:40 -0400

    Update Waiting component styles for improved UI consistency

--- app/stages/2-waiting-rival.tsx
+++ app/stages/2-waiting-rival.tsx
@@ -1,3 +1,4 @@
+import { GiDoubled } from "react-icons/gi";
 import { useGame } from "../context/game-context";
 
 interface Props {
@@ -14,13 +15,18 @@ function Waiting({ code, setCode }: Props) {
   }
 
   return (
-    <section className="rounded-xl p-4 flex flex-col items-center gap-8">
-      <h4 className="text-primary font-bold text-center">Waiting for rival</h4>
-      <h4 className="text-primary font-bold text-center text-3xl gradient-text">
+    <section className="rounded-xl p-4 flex flex-col items-center gap-6">
+      <h4 className="text-primary/80 font-bold text-center">
+        Waiting for rival to join
+      </h4>
+      <div className="text-primary animate-spin">
+        <GiDoubled size={40} />
+      </div>
+      <h4 className="text-primary font-bold text-center text-4xl gradient-text">
         {code}
       </h4>
       <button
-        className="bg-primary hover:bg-primary/80 text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit"
+        className="border-2 border-primary text-primary hover:bg-primary hover:text-backgroundSecondary px-4 py-2 rounded-lg font-bold w-fit"
         onClick={finishMatch}
       >
         Cancel
