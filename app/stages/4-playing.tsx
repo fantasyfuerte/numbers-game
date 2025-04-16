7e009c9a7d3b5ce105309108fcebe513644128cc
Fuerte <leandrofuerte75@gmail.com>
2025-04-15 21:42:05 -0400

    getting number

--- app/stages/4-playing.tsx
+++ app/stages/4-playing.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { useGame } from "../context/game-context";
+import { Numbers } from "../components/number-setter.tsx";
 
 function Playing() {
+  const [number, setNumber] = useState<string>();
   const { isMyTurn } = useGame();
+
+  function Submit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    alert(number);
+  }
 
   return (
     <section className="flex flex-col gap-10 items-center">
       <h4 className="text-xl text-primary font-bold">
-        {isMyTurn ? "It's your turn" : "The rival is thinking fons"}
+        {isMyTurn ? "It's your turn" : "The rival is thinking"}
       </h4>
+      <form onSubmit={Submit}>
+        <Numbers onNumberChange={setNumber} />
+      </form>
     </section>
   );
 }
