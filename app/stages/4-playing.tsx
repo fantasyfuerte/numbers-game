f0948d60acbbb583566285944ac582a242c532c3
Fuerte <leandrofuerte75@gmail.com>
2025-04-16 18:02:57 -0400

    Refactor Playing component to enhance layout and improve visual consistency for notes display

--- app/stages/4-playing.tsx
+++ app/stages/4-playing.tsx
@@ -47,7 +47,7 @@ function Playing({ code }: Props) {
         </button>
       </form>
       <article className="grid grid-cols-2 gap-10">
-        <ul className="flex flex-col gap-4">
+        <ul className="flex flex-col gap-4 items-center">
           <h4 className="text-primary font-bold text-center">You</h4>
           {notes.you.map((note, index) => (
             <li className="flex gap-2 items-center text-primary" key={index}>
@@ -59,13 +59,18 @@ function Playing({ code }: Props) {
             </li>
           ))}
         </ul>
-        <div>
+        <ul className="flex flex-col gap-4 items-center">
+          <h4 className="text-primary font-bold text-center">Rival</h4>
           {notes.rival.map((note, index) => (
-            <p key={index} className="text-primary font-bold">
-              {note[0]}: {note[1]}
-            </p>
+            <li className="flex gap-2 items-center text-primary" key={index}>
+              <div className="bg-gradient-to-r from-primary to-secondary rounded-lg p-2 text-backgroundSecondary font-bold">
+                {note[0]}
+              </div>
+              <p className="font-semibold -mr-[6px]">{note[1]} </p>
+              <GiCheckMark size={15} />
+            </li>
           ))}
-        </div>
+        </ul>
       </article>
     </section>
   );
