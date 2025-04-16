e13b4b0ea17cf991592634ba7836a3070c501328
Fuerte <leandrofuerte75@gmail.com>
2025-04-16 11:29:39 -0400

    Implement results display in Results component

--- app/stages/5-results.tsx
+++ app/stages/5-results.tsx
@@ -1,6 +1,14 @@
 import { useGame } from "../context/game-context";
+
+const { results } = useGame();
 function Results() {
-  return <section></section>;
+  return (
+    <section>
+      <h4 className="text-xl text-primary font-bold">
+        {results == "winner" ? "You won!" : "You lost!"}
+      </h4>
+    </section>
+  );
 }
 
 export default Results;
