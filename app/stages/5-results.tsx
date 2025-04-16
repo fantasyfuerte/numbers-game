6375b57c358c0f17d2f73bb5d60243b31a2fa60b
Fuerte <leandrofuerte75@gmail.com>
2025-04-16 17:13:25 -0400

    Add setCode prop to Results component for better state management

--- app/stages/5-results.tsx
+++ app/stages/5-results.tsx
@@ -2,19 +2,23 @@ import { useGame } from "../context/game-context";
 
 interface Props {
   code: string;
+  setCode: (code: string) => void;
 }
 
-function Results({ code }: Props) {
+function Results({ code, setCode }: Props) {
   const { results, finishGame } = useGame();
 
   return (
-    <section>
-      <h4 className="text-xl text-primary font-bold">
+    <section className="flex flex-col gap-8 items-center">
+      <h4 className="text-2xl text-primary font-bold">
         {results == "winner" ? "You won!" : "You lost!"}
       </h4>
       <button
-        onClick={() => finishGame(code)}
-        className="bg-primary text-white px-4 py-2 rounded-md"
+        onClick={() => {
+          finishGame(code);
+          setCode("");
+        }}
+        className="bg-primary text-backgroundSecondary font-bold px-4 py-2 rounded-lg w-fit"
       >
         Play again
       </button>
