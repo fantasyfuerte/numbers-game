fb036750f9e9461f2d7c586bf7ff3c39fb6036ef
Fuerte <leandrofuerte75@gmail.com>
2025-04-16 21:44:04 -0400

    Refactor Results component to enhance visual feedback with Confetti and improve text styling

--- app/stages/5-results.tsx
+++ app/stages/5-results.tsx
@@ -1,4 +1,6 @@
+import { GiDeadHead, GiPodiumWinner } from "react-icons/gi";
 import { useGame } from "../context/game-context";
+import Confetti from "react-confetti";
 
 interface Props {
   code: string;
@@ -9,7 +11,15 @@ function Results({ code, setCode }: Props) {
   const { results, finishGame } = useGame();
 
   return (
-    <section className="flex flex-col gap-8 items-center">
+    <section className="flex flex-col gap-8 items-center text-primary">
+      {results == "winner" ? (
+        <>
+          <GiPodiumWinner size={100} />
+          <Confetti colors={["#dba465", "#c42626"]} />
+        </>
+      ) : (
+        <GiDeadHead size={100} />
+      )}
       <h4 className="text-2xl text-primary font-bold">
         {results == "winner" ? "You won!" : "You lost!"}
       </h4>
